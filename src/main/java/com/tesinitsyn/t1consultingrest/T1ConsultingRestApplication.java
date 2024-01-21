3fbdfc0bffdc7a638d5f74d8d6f543fbaba182f0
tesinitsyn <sitiev174@gmail.com>
2024-01-21 21:06:18 +0300

    Initial commit

---
+++ src/main/java/com/tesinitsyn/t1consultingrest/T1ConsultingRestApplication.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.t1consultingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1ConsultingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1ConsultingRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesinitsyn/t1consultingrest/T1ConsultingRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesinitsyn.t1consultingrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1ConsultingRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
