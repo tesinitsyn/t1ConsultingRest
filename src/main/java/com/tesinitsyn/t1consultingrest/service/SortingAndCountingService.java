02e061f558c6ad43b882e8c6607587634c1c16d0
tesinitsyn <sitiev174@gmail.com>
2024-01-21 21:17:42 +0300

    Added countCharacters logic

--- src/main/java/com/tesinitsyn/t1consultingrest/service/SortingAndCountingService.java
+++ src/main/java/com/tesinitsyn/t1consultingrest/service/SortingAndCountingService.java
@@ -1,7 +1,19 @@
 package com.tesinitsyn.t1consultingrest.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SortingAndCountingService {
     public String countCharacters(String input){
-        return "";
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char c : input.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        StringBuilder result = new StringBuilder();
+        charCount.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                .forEach(entry -> result.append("\"").append(entry.getKey()).append("\": ").append(entry.getValue()).append(", "));
+        result.delete(result.length() - 2, result.length());
+        return result.toString();
     }
 }
